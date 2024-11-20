30abb8cbc3f4701560d2c90000719121dca596bd
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-11-21 02:09:23 +0530

    trying again

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ def get_transcript(url):
     video_id = get_youtube_video_id(url)
     img_url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
     # transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, proxies={"https": "http://localhost:8080"})
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id, proxies={"https": "http://209.121.164.50:31147"})
     transcript_text = " ".join(entry['text'] for entry in transcript_list)
     return [transcript_text, img_url]
 
