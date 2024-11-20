2c5f1a50c2894ea4b667357bb7ece70457ff3eac
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-11-21 00:46:00 +0530

    port changed

--- backend/main.py
+++ backend/main.py
@@ -62,4 +62,5 @@ def home():
     return jsonify({"error": "No YouTube URL provided."}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
